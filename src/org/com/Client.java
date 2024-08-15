a7d619209682ed45c9b46f4395f67f600ef38ae0
Mincy6391 <minz.jema@gmail.com>
2024-08-15 16:05:10 +0530

    QA

--- src/org/com/Client.java
+++ src/org/com/Client.java
@@ -4,6 +4,8 @@ public abstract class Client  {
 int a =10;
 public void empName() {
 	System.out.println("siva");
+	System.out.println("Sumathi");
+	System.out.println("AnuRithu");
 }
 
 	public abstract void empId();
