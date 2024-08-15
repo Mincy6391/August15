666e4003425a5387a2c84f05dc24f3cbb307024b
Mincy6391 <minz.jema@gmail.com>
2024-08-15 15:19:57 +0530

    task revise

---
+++ src/org/com/Client.java
@@ -0,0 +1,11 @@
+package org.com;
+
+public abstract class Client  {
+int a =10;
+public void empName() {
+	System.out.println("siva");
+}
+
+	public abstract void empId();
+}
+
---
+++ src/org/com/Employee.java
@@ -0,0 +1,12 @@
+package org.com;
+
+public class Employee {
+	 
+public static void empName() {
+	System.out.println("siva");
+}
+public static void main(String[] args) {
+	empName();
+}
+
+}
